6e93c75df0d93c35c2c0e64033202ec51ce1791d
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2025-02-12 16:07:37 +0100

    Remove obsolete files and add requirements.txt for project dependencies

--- install/lomas_interfaces/share/lomas_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(lomas_interfaces_IDL_FILES "msg/MachineCommand.idl;msg/MachineStatus.idl")
-set(lomas_interfaces_INTERFACE_FILES "msg/MachineCommand.msg;msg/MachineStatus.msg")
